b155c40d293db8413ec8ae4cedc0fc40334acc8a
sanibalakrishna <sanibalakrishna@gmail.com>
2023-03-03 12:46:45 +0530

    doctor timings

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,89 +1,87 @@
 const { default: mongoose } = require("mongoose");
-const Doctor = require("../models/doctorModel")
+const Doctor = require("../models/doctorModel");
 
 // get all doctors
-const getAllDoctors = async(req,res)=>{ 
-    const doctor= await Doctor.find();
-    res.status(200).json(doctor);
-}
+const getAllDoctors = async (req, res) => {
+  const doctor = await Doctor.find();
+  res.status(200).json(doctor);
+};
 
 // get a specific doctor
-const getDoctor = async(req,res)=>{
-    const {id} = req.params;
-    if(!mongoose.Types.ObjectId.isValid(id))
-    {
-       return res.status(404).json({message:"no such doctor found"});
-    }
-    const doctor = await Doctor.findById(id);
-    if(!doctor)
-    {
-       return res.status(404).json({message:"no such doctor found"});
-    }
-    res.status(200).json(doctor);
-}
-
+const getDoctor = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ message: "no such doctor found" });
+  }
+  const doctor = await Doctor.findById(id);
+  if (!doctor) {
+    return res.status(404).json({ message: "no such doctor found" });
+  }
+  res.status(200).json(doctor);
+};
 
 // create a doctor
-const createDoctor =async (req,res)=>{
-    const {name,specilization} = req.body;
-    const emptyfiels= []
-    if(!name)
-    {
-        emptyfiels.push("name");
-    }
-    if(!specilization)
-    {
-        emptyfiels.push("specilization");
-    }
-  
-    
-    if(emptyfiels.length>0)
-    {
-        return res.status(400).json({message:"Please fill all the fields",fields:emptyfiels})
-    }
+const createDoctor = async (req, res) => {
+  const { name, specilization, arriveTime, departTime } = req.body;
+  const emptyfiels = [];
+  if (!name) {
+    emptyfiels.push("name");
+  }
+  if (!specilization) {
+    emptyfiels.push("specilization");
+  }
+  if (!arriveTime) {
+    emptyfiels.push("arriveTime");
+  }
+  if (!departTime) {
+    emptyfiels.push("departTime");
+  }
 
-    try {
-        const doctor = new Doctor({name,specilization});
-        doctor.save();
-        res.status(200).json(doctor)
-        
-    } catch (error) {
-        res.status(404).json({message:error.message});
-        
-    }
+  if (emptyfiels.length > 0) {
+    return res
+      .status(400)
+      .json({ message: "Please fill all the fields", fields: emptyfiels });
+  }
 
-
-}
-
+  try {
+    const doctor = new Doctor({ name, specilization, arriveTime, departTime });
+    doctor.save();
+    res.status(200).json(doctor);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 // update a doctor
-const updateDoctor = async(req,res)=>{
- const {id} = req.params;
- if(!mongoose.Types.ObjectId.isValid(id))
- {
-    return res.status(404).json({message:"no such doctor found"});
- }
- const doctor = await Doctor.findOneAndUpdate({_id:id},req.body);
- if(!doctor)
- {
-    return res.status(404).json({message:"no such doctor found"});
- }
- res.status(200).json(doctor);
-}
+const updateDoctor = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ message: "no such doctor found" });
+  }
+  const doctor = await Doctor.findOneAndUpdate({ _id: id }, req.body);
+  if (!doctor) {
+    return res.status(404).json({ message: "no such doctor found" });
+  }
+  res.status(200).json(doctor);
+};
 
 // delete a doctor
-const deleteDoctor = async(req,res)=>{
-    const {id} = req.params;
-    if(!mongoose.Types.ObjectId.isValid(id))
-    {
-       return res.status(404).json({message:"no such doctor found"});
-    }
-    const doctor = await Doctor.findOneAndDelete({_id:id});
-    if(!doctor)
-    {
-       return res.status(404).json({message:"no such doctor found"});
-    }
-    res.status(200).json(doctor);
-}
+const deleteDoctor = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ message: "no such doctor found" });
+  }
+  const doctor = await Doctor.findOneAndDelete({ _id: id });
+  if (!doctor) {
+    return res.status(404).json({ message: "no such doctor found" });
+  }
+  res.status(200).json(doctor);
+};
 
-module.exports={getDoctor,createDoctor,getAllDoctors,updateDoctor,deleteDoctor}
+module.exports = {
+  getDoctor,
+  createDoctor,
+  getAllDoctors,
+  updateDoctor,
+  deleteDoctor,
+};
--- models/doctorModel.js
+++ models/doctorModel.js
@@ -1,19 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const doctorSchema = new Schema({
-    name:{
-        type:String,
-        required:[true,"Doctor should have a name"]
-    },
-    specilization:{
-        type:String,
-        required:[true,"Doctor should have a specilization"]
-    },
-    sessionsbooked:{
-        type:[String],
+  name: {
+    type: String,
+    required: [true, "Doctor should have a name"],
+  },
+  specilization: {
+    type: String,
+    required: [true, "Doctor should have a specilization"],
+  },
+  arriveTime: {
+    type: String,
+    required: [true, "Doctor should have arrival time"],
+  },
+  departTime: {
+    type: String,
+    required: [true, "Doctor should have depart time"],
+  },
+});
 
-    }
-})
-
-module.exports = mongoose.model('Doctor',doctorSchema);
+module.exports = mongoose.model("Doctor", doctorSchema);
